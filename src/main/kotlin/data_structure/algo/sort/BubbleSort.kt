c926aa2cf70dba74b230c24b1030eb92519ff58e
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-12-04 15:53:06 +0900

    feat: BubbleSort

---
+++ src/main/kotlin/data_structure/algo/sort/BubbleSort.kt
@@ -0,0 +1,17 @@
+package data_structure.algo.sort
+
+class BubbleSort {
+    companion object {
+        fun sort(arr: Array<Int>) {
+            for (i in arr.indices) {
+                for (j in 0  until arr.size - i - 1) {
+                    if (arr[j] > arr[j + 1]) {
+                        val temp = arr[j]
+                        arr[j] = arr[j + 1]
+                        arr[j + 1] = temp
+                    }
+                }
+            }
+        }
+    }
+}
